d471341107c64c61abf01d608443d1f6478c85bc
xandao.dev <alexandrevitaldp@gmail.com>
2025-12-18 21:15:27 -0300

    começando a implementar a classe transacao

---
+++ src/br/com/criptovision/model/Transacao.java
@@ -0,0 +1,28 @@
+package br.com.criptovision.model;
+
+import java.time.LocalDateTime;
+
+public class Transacao {
+    private String ticker; // moeda (btc, eth...)
+    private double quantidade;
+    private double precoUnitario;
+    private LocalDateTime data;
+    private String tipo;  // se vai ser compra ou venda
+
+    //construtor
+    public Transacao(String ticker, double quantidade, double precoUnitario, String tipo){
+        this.ticker = ticker.toUpperCase();
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+        this.tipo = tipo.toUpperCase();
+        this.data = LocalDateTime.now();
+    }
+    
+    //getters e setters
+    public String getTicker(){ return ticker; }
+    public double getQuantidade(){ return quantidade; }
+    public double getPrecoUnitario(){ return precoUnitario; }
+    public String getTipo(){ return tipo; }
+    public LocalDateTime getData(){ return data; }
+
+}
