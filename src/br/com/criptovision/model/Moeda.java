1d495d4a4b7f055da12a826a8d5c23c4cd3ca56d
xandao.dev <alexandrevitaldp@gmail.com>
2025-12-18 22:13:54 -0300

    criando a classe da carteira, fundindo a moeda com a transaçao

---
+++ src/br/com/criptovision/model/Moeda.java
@@ -0,0 +1,22 @@
+package br.com.criptovision.model;
+
+public class Moeda {
+    private String nome; // "bitcoin"
+    private String ticker;// "BTC"
+    private double saldo;
+    private double precoMedio;
+
+    public Moeda(String nome, String ticker){
+        this.nome = nome;
+        this.ticker = ticker.toUpperCase();
+        this.saldo = 0.0;
+        this.precoMedio = 0.0;
+    }
+    
+    //getter e setters
+    public String getTicker(){ return ticker; }
+    public double getSaldo(){ return saldo; }
+    public void setSaldo(double saldo){ this.saldo = saldo; }
+    public double getPrecoMedio(){ return precoMedio; }
+    public void setPrecoMedio(double precoMedio){ this.precoMedio = precoMedio; }
+}
---
+++ src/br/com/criptovision/service/CarteiraService.java
@@ -0,0 +1,27 @@
+package br.com.criptovision.service;
+
+import br.com.criptovision.model.Moeda;
+import br.com.criptovision.model.Transacao;
+
+public class CarteiraService {
+
+    public void ProcessarTransacao(Moeda moeda, Transacao transacao){
+        if(transacao.getTipo().equals("COMPRA")){
+            double custoTotalAntigo = moeda.getSaldo() * moeda.getPrecoMedio();
+            double custoNovaCompra = transacao.getQuantidade() * transacao.getPrecoUnitario();
+
+            double novoSaldo = moeda.getSaldo() + transacao.getQuantidade();
+            double novoPrecoMedio = (custoTotalAntigo + custoNovaCompra) / novoSaldo;
+
+            //atualizando os valores de saldo e preco medio
+            moeda.setSaldo(novoSaldo);
+            moeda.setPrecoMedio(novoPrecoMedio);
+
+        }else if(transacao.getTipo().equals("VENDA")){
+            moeda.setSaldo(moeda.getSaldo() - transacao.getQuantidade());
+            //na venda o preço medio nao muda
+            
+        }
+    }
+    
+}
