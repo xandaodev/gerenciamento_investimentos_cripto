947389aa9d564c720b880f6969bf30695a4513b5
xandao.dev <alexandrevitaldp@gmail.com>
2025-12-20 16:22:38 -0300

    criando a primeira main interativa

--- src/br/com/criptovision/main/Main.java
+++ src/br/com/criptovision/main/Main.java
@@ -3,29 +3,67 @@ package br.com.criptovision.main;
 import br.com.criptovision.model.Moeda;
 import br.com.criptovision.model.Transacao;
 import br.com.criptovision.service.CarteiraService;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
-        CarteiraService service = new CarteiraService();
-        
-        // adicionando uma moeda na carteira
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        CarteiraService carteira = new CarteiraService();
+        //criando um array pro historico
+        List<Transacao> historico = new ArrayList<>();
+
+        //focando apenas no bitcoin por enquango
         Moeda btc = new Moeda("Bitcoin", "BTC");
 
-        // primeira compra: 1 BTC a 86.000
-        Transacao t1 = new Transacao("BTC", 1, 86000.00, "COMPRA");
-        service.processarTransacao(btc, t1);
-        exibirStatus(btc);
+        int opcao = 0;
+        while (opcao != 4){
+            System.out.println("\n--- GERENCIAMNETO DE INVESTIMENTOS CRIPTO ---");
+            System.out.println("1. Nova compra");
+            System.out.println("2. Ver Saldo e Preço Médio");
+            System.out.println("3. Ver Histórico de Transações");
+            System.out.println("4. Sair");
+            System.out.print("Escolha uma opção: ");
+            
+            opcao = leitor.nextInt();
 
-        // segunda compra: 1 BTC a 92.000
-        Transacao t2 = new Transacao("BTC", 1, 92000.00, "COMPRA");
-        service.processarTransacao(btc, t2);
-        exibirStatus(btc);
-    }
+            switch (opcao){
+                case 1:
+                    System.out.print("Quantidade comprada: ");
+                    double qtd = leitor.nextDouble();
+                    System.out.print("Preço unitário pago: ");
+                    double preco = leitor.nextDouble();
+                    
+                    Transacao t = new Transacao("BTC", qtd, preco, "COMPRA");
+                    carteira.processarTransacao(btc, t);
+                    historico.add(t); // guarda no histórico
+                    
+                    System.out.println(" Compra registrada com sucesso!");
+                    break;
+                    
+                case 2:
+                    System.out.println("\n--- MEU SALDO ---");
+                    System.out.println("Ativo: " + btc.getTicker());
+                    System.out.println("Quantidade: " + btc.getSaldo());
+                    System.out.printf("Preço Médio: R$ %.2f\n", btc.getPrecoMedio());
+                    break;
 
-    public static void exibirStatus(Moeda moeda) {
-        System.out.println("Saldo: " + moeda.getSaldo() + " " + moeda.getTicker());
-        System.out.printf("Preço Médio: R$ %.2f\n", moeda.getPrecoMedio());
-        System.out.print("\n");
+                case 3:
+                    System.out.println("\n--- HISTÓRICO ---");
+                    for(Transacao tr : historico){
+                        System.out.println(tr.getTipo() + " | " + tr.getQuantidade() + " BTC | R$ " + tr.getPrecoUnitario());
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+        leitor.close();
     }
-    
 }
