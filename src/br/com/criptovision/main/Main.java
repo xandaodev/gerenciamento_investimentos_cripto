e38e712a44407ff816be78d0ad0c68a813c4530e
xandao.dev <alexandrevitaldp@gmail.com>
2025-12-24 00:39:25 -0300

    agora api funcionando, o programa puxa o preço atual da moeda em tempo real

--- src/br/com/criptovision/main/Main.java
+++ src/br/com/criptovision/main/Main.java
@@ -4,6 +4,7 @@ import br.com.criptovision.model.Carteira;
 import br.com.criptovision.model.Moeda;
 import br.com.criptovision.model.Transacao;
 import br.com.criptovision.service.CarteiraService;
+import br.com.criptovision.service.HttpService;
 import br.com.criptovision.repository.TransacaoRepository;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -28,100 +29,112 @@ public class Main {
 
         int opcao = 0;
         while (opcao != 6){
-            System.out.println("\n--- GERENCIAMNETO DE INVESTIMENTOS CRIPTO ---");
-            System.out.println("1. Nova compra");
-            System.out.println("2. Ver Saldo e Preço Médio");
-            System.out.println("3. Ver Histórico de Transações");
-            System.out.println("4. Nova venda");
-            System.out.println("5. Simular lucro");
-            System.out.println("6. Sair do sistema");
-            System.out.print("Escolha uma opção: ");
-            
-            opcao = leitor.nextInt();
+            try{
+                System.out.println("\n--- GERENCIAMNETO DE INVESTIMENTOS CRIPTO ---");
+                System.out.println("1. Nova compra");
+                System.out.println("2. Ver Saldo e Preço Médio");
+                System.out.println("3. Ver Histórico de Transações");
+                System.out.println("4. Nova venda");
+                System.out.println("5. Simular lucro");
+                System.out.println("6. Sair do sistema");
+                System.out.print("Escolha uma opção: ");
+                
+                opcao = leitor.nextInt();
 
-            switch (opcao){
-                case 1:
-                    System.out.print("Qual o Ticker da moeda ? ");
-                    String ticker = leitor.next().toUpperCase();
+                switch (opcao){
+                    case 1:
+                        System.out.print("Qual o Ticker da moeda ? ");
+                        String ticker = leitor.next().toUpperCase();
 
-                    // se não existir, é criada a moeda na hora
-                    Moeda moedaSelecionada = minhaCarteira.obterMoeda(ticker, ticker); 
+                        // se não existir, é criada a moeda na hora
+                        Moeda moedaSelecionada = minhaCarteira.obterMoeda(ticker, ticker); 
 
-                    System.out.print("Quantidade comprada: ");
-                    double qtd = leitor.nextDouble();
-                    System.out.print("Preço unitário pago: ");
-                    double preco = leitor.nextDouble();
-                    
-                    Transacao t = new Transacao(ticker, qtd, preco, "COMPRA");
-                    carteira.processarTransacao(moedaSelecionada, t);
-                    historico.add(t); // guarda no histórico
-                    repositorio.salvar(t);
-                    System.out.println(" Compra registrada com sucesso!");
-                    break;
-                    
-                case 2:
-                    System.out.println("\n--- MINHAS MOEDAS ---");
-                    for(Moeda m : minhaCarteira.getMoedas().values()){
-                        if(m.getSaldo() > 0){
-                            System.out.printf("Ativo: %s | Saldo: %.8f | Preço Médio: $ %.2f\n", 
-                                m.getTicker(), m.getSaldo(), m.getPrecoMedio());
+                        System.out.print("Quantidade comprada: ");
+                        double qtd = leitor.nextDouble();
+                        System.out.print("Preço unitário pago: ");
+                        double preco = leitor.nextDouble();
+                        
+                        Transacao t = new Transacao(ticker, qtd, preco, "COMPRA");
+                        carteira.processarTransacao(moedaSelecionada, t);
+                        historico.add(t); // guarda no histórico
+                        repositorio.salvar(t);
+                        System.out.println(" Compra registrada com sucesso!");
+                        break;
+                        
+                    case 2:
+                        System.out.println("\n--- MINHAS MOEDAS ---");
+                        for(Moeda m : minhaCarteira.getMoedas().values()){
+                            if(m.getSaldo() > 0){
+                                System.out.printf("Ativo: %s | Saldo: %.8f | Preço Médio: $ %.2f\n", 
+                                    m.getTicker(), m.getSaldo(), m.getPrecoMedio());
+                            }
                         }
-                    }
-                    break;
+                        break;
 
-                case 3:
-                    System.out.println("\n--- HISTÓRICO ---");
-                    for(Transacao tr : historico){
-                        System.out.println(tr.getTipo() + " | " + tr.getTicker() + " | " + tr.getQuantidade() + " | $ " + tr.getPrecoUnitario());
-                    }
-                    break;
-
-                case 4:
-                    System.out.print("Qual o Ticker da moeda para venda? ");
-                    String tickerVenda = leitor.next().toUpperCase();
-                    Moeda moedaVenda = minhaCarteira.obterMoeda(tickerVenda, tickerVenda);
-
-                    System.out.print("Quantidade vendida: ");
-                    double qtdVenda = leitor.nextDouble();
-                    if(qtdVenda > moedaVenda.getSaldo()){
-                        System.out.println("Saldo insuficiente!");
-                    }else{
-                    System.out.print("Preço unitário de venda: ");
-                    double precoVenda = leitor.nextDouble();
-                    
-                    Transacao tVenda = new Transacao(tickerVenda, qtdVenda, precoVenda, "VENDA");
-                    carteira.processarTransacao(moedaVenda, tVenda);
-                    historico.add(tVenda); // guarda no histórico
-                    
-                    repositorio.salvar(tVenda);//salvando no repository
-                    System.out.println(" Venda registrada com sucesso!");
-                    }
-                    break;
-                case 5:
-                    System.out.println("\n--- SIMULADOR DE LUCRO ---");
-                    for (Moeda m : minhaCarteira.getMoedas().values()) {
-                        if (m.getSaldo() > 0) {
-                            System.out.print("Digite o preço atual de mercado para " + m.getTicker() + ": ");
-                            double precoMercado = leitor.nextDouble();
-                            
-                            double lucro = carteira.calcularLucroPotencial(m, precoMercado);
-                            double porcentagem = (lucro / (m.getSaldo() * m.getPrecoMedio())) * 100;
-
-                            System.out.printf("Resultado para %s:\n", m.getTicker());
-                            System.out.printf("  Saldo atual: %.8f\n", m.getSaldo());
-                            System.out.printf("  PNL: $ %.2f (%.2f%%)\n", lucro, porcentagem);
-                            System.out.println("---------------------------------------");
+                    case 3:
+                        System.out.println("\n--- HISTÓRICO ---");
+                        for(Transacao tr : historico){
+                            System.out.println(tr.getTipo() + " | " + tr.getTicker() + " | " + tr.getQuantidade() + " | $ " + tr.getPrecoUnitario());
                         }
-                    }
-                    break;
-                case 6:
-                    System.out.println("Saindo...");
-                    break;
+                        break;
 
-                default:
-                    System.out.println("Opção inválida!");
+                    case 4:
+                        System.out.print("Qual o Ticker da moeda para venda? ");
+                        String tickerVenda = leitor.next().toUpperCase();
+                        Moeda moedaVenda = minhaCarteira.obterMoeda(tickerVenda, tickerVenda);
+
+                        System.out.print("Quantidade vendida: ");
+                        double qtdVenda = leitor.nextDouble();
+                        if(qtdVenda > moedaVenda.getSaldo()){
+                            System.out.println("Saldo insuficiente!");
+                        }else{
+                        System.out.print("Preço unitário de venda: ");
+                        double precoVenda = leitor.nextDouble();
+                        
+                        Transacao tVenda = new Transacao(tickerVenda, qtdVenda, precoVenda, "VENDA");
+                        carteira.processarTransacao(moedaVenda, tVenda);
+                        historico.add(tVenda); // guarda no histórico
+                        
+                        repositorio.salvar(tVenda);//salvando no repository
+                        System.out.println(" Venda registrada com sucesso!");
+                        }
+                        break;
+                    case 5:
+                        System.out.println("\n--- SIMULADOR DE LUCRO ---");
+
+                        HttpService http = new HttpService();
+
+                        for (Moeda m : minhaCarteira.getMoedas().values()) {
+                            if (m.getSaldo() > 0) {
+
+                                double precoMercado = http.buscarPrecoAtual(m.getTicker());
+                                //double precoMercado = leitor.nextDouble();
+                                
+                                double lucro = carteira.calcularLucroPotencial(m, precoMercado);
+                                double porcentagem = (lucro / (m.getSaldo() * m.getPrecoMedio())) * 100;
+
+                                System.out.printf("Resultado para %s:\n", m.getTicker());
+                                System.out.printf("  Saldo atual: %.8f\n", m.getSaldo());
+                                System.out.printf("  Preço atual: %.8f\n", precoMercado);
+                                System.out.printf("  PNL: $ %.2f (%.2f%%)\n", lucro, porcentagem);
+                                System.out.println("---------------------------------------");
+                            }
+                        }
+                        break;
+                    case 6:
+                        System.out.println("Saindo...");
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida!");
+                }
+            }catch(NumberFormatException e){ 
+                System.out.println(" ERRO: Por favor, digite apenas números inteiros para as opções do menu.");
+            }catch(Exception e){ 
+                System.out.println(" Ocorreu um erro inesperado: " + e.getMessage());
             }
         }
+
         leitor.close();
     }
 }
--- src/br/com/criptovision/service/HttpService.java
+++ src/br/com/criptovision/service/HttpService.java
@@ -1,5 +1,47 @@
 package br.com.criptovision.service;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class HttpService {
+
+    public double buscarPrecoAtual(String ticker) {
+        // convertendo o sticker pro nome correto 
+        String idMoeda = converterTickerParaId(ticker);
+        
+        String url = "https://api.coingecko.com/api/v3/simple/price?ids=" + idMoeda + "&vs_currencies=usd";
+
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            String valorString = json.split(":")[2].replace("}}", "");
+            
+            return Double.parseDouble(valorString);
+
+        }catch (Exception e){
+            System.out.println("Erro ao buscar preço para " + ticker + ". Verifique sua conexão com a internet.");
+            return 0;
+        }
+    }
+
+    //funcao para converter o ticket para o nome que a api usa
+    private String converterTickerParaId(String ticker) {
+        return switch (ticker.toUpperCase()) {
+            case "BTC" -> "bitcoin";
+            case "ETH" -> "ethereum";
+            case "SOL" -> "solana";
+            case "LNK" -> "chainlink";
+            default -> ticker.toLowerCase();
+        };
+    }
     
 }
