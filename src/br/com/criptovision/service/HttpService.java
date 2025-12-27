7aaa68109677ef8c07244d77cc279fa448909799
xandao.dev <alexandrevitaldp@gmail.com>
2025-12-26 21:36:29 -0300

    adicionando tratamentos de excecoes na classe que chama a api

--- src/br/com/criptovision/service/HttpService.java
+++ src/br/com/criptovision/service/HttpService.java
@@ -5,6 +5,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import java.time.Duration;
+
 public class HttpService {
 
     public double buscarPrecoAtual(String ticker) {
@@ -14,23 +16,40 @@ public class HttpService {
         String url = "https://api.coingecko.com/api/v3/simple/price?ids=" + idMoeda + "&vs_currencies=usd";
 
         try{
-            HttpClient client = HttpClient.newHttpClient();
+            HttpClient client = HttpClient.newBuilder()
+                    .connectTimeout(Duration.ofSeconds(10))
+                    .build();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(url))
+                    .timeout(Duration.ofSeconds(10))
                     .GET()
                     .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+            if (response.statusCode() != 200){
+                System.out.println("API retornou algum erro: " + response.statusCode());
+                return 0;
+            }
+
             String json = response.body();
+            // verificaçao:  se o JSON for {}, significa que a moeda nao foi encontrada
+            if (json.equals("{}")){
+                System.out.println("Moeda '" + ticker + "' nao encontrada na API");
+                return 0;
+            }
             String valorString = json.split(":")[2].replace("}}", "");
             
             return Double.parseDouble(valorString);
 
-        }catch (Exception e){
-            System.out.println("Erro ao buscar preço para " + ticker + ". Verifique sua conexão com a internet.");
-            return 0;
+        }catch(java.net.http.HttpTimeoutException e){
+            System.out.println("Erro: Tempo limite esgotado. Talvez a internet esteja lenta");
+        }catch(java.net.ConnectException e){
+            System.out.println("Erro: Falha ao conectar. Talvez voce esteja sem internet");
+        }catch(Exception e){
+            System.out.println("Erro inesperado na consulta: " + e.getMessage());
         }
+        return 0;
     }
 
     //funcao para converter o ticket para o nome que a api usa
