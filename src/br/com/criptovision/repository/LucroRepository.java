e22ad404d0e958cbaf9641f9d78f3cde2085eb3a
xandao.dev <alexandrevitaldp@gmail.com>
2026-01-22 11:08:18 -0300

    criacao da nova classe que sera responsavel por criar o arquivo para armazenar os lucros realizados

---
+++ src/br/com/criptovision/repository/LucroRepository.java
@@ -0,0 +1,11 @@
+package br.com.criptovision.repository;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+public class LucroRepository {
+    
+}
