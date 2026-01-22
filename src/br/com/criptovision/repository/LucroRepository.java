9703da806dbaaa18ccaf75e0fafe33c658daebb1
xandao.dev <alexandrevitaldp@gmail.com>
2026-01-22 11:09:46 -0300

    atualizando nova classe

--- src/br/com/criptovision/repository/LucroRepository.java
+++ src/br/com/criptovision/repository/LucroRepository.java
@@ -7,5 +7,13 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
 public class LucroRepository {
-    
+    private String nomeArquivo = "lucros.csv";
+    public void salvarLucroRealizado(String ticker, double valorLucro){
+        try(FileWriter fw = new FileWriter(nomeArquivo, true);PrintWriter pw = new PrintWriter(fw)){
+            // salva valor, ticker e data
+            pw.println(ticker + "," + valorLucro + "," + LocalDateTime.now());
+        }catch(IOException e){
+            System.out.println("Erro ao registrar lucro: " + e.getMessage());
+        }
+    }
 }
