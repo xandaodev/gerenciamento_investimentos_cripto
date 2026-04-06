56ad2fbc6dac92e7c8990cb7c94343322c51e2ba
xandao.dev <alexandrevitaldp@gmail.com>
2026-04-05 21:26:59 -0300

    ajustando o vizualizarcarteira.bat

--- Visualizar_Carteira.bat
+++ Visualizar_Carteira.bat
@@ -1,3 +1,7 @@
 @echo off
-java -jar CriptoResumo.jar
+echo Compilando o sistema mais recente na versao atualizada..
+javac -d bin -cp "lib/*" -sourcepath src src/br/com/criptovision/main/Main.java
+
+echo Abrindo Menu Principal...
+java -cp "bin;lib/*" br.com.criptovision.main.Main
 pause
