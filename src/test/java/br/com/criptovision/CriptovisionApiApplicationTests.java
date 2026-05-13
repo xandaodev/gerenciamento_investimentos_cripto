65dd1a9def921c02c079ba3cb247e054590a30eb
xandao.dev <alexandrevitaldp@gmail.com>
2026-05-13 11:03:49 -0300

    migração completa da arquitetura para Spring Boot e Maven

---
+++ src/test/java/br/com/criptovision/CriptovisionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.criptovision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptovisionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
