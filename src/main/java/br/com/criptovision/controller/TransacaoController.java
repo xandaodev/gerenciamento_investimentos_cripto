ae970ef59ae2c4bd0ff95f01b75ae4ed7d1bfac4
xandao.dev <alexandrevitaldp@gmail.com>
2026-05-13 14:18:35 -0300

    adicionando o metodo get buscarPorId em TransacaoController

--- src/main/java/br/com/criptovision/controller/TransacaoController.java
+++ src/main/java/br/com/criptovision/controller/TransacaoController.java
@@ -3,9 +3,11 @@ package br.com.criptovision.controller;
 import br.com.criptovision.model.Transacao;
 import br.com.criptovision.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/transacoes")
@@ -23,5 +25,16 @@ public class TransacaoController {
     public Transacao salvar(@RequestBody Transacao novaTransacao) {
 
         return repository.save(novaTransacao);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transacao> buscarPorId(@PathVariable Long id) {
+
+        Optional<Transacao> transacao = repository.findById(id);
+
+        if (transacao.isPresent()) {
+            return ResponseEntity.ok(transacao.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
