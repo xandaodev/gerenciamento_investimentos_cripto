f2be4fdd9f7dd3e12613f9416ad46886b5e58794
xandao.dev <alexandrevitaldp@gmail.com>
2026-05-13 15:30:33 -0300

    implementando CarteiraController

--- src/main/java/br/com/criptovision/controller/CarteiraController.java
+++ src/main/java/br/com/criptovision/controller/CarteiraController.java
@@ -1,4 +1,21 @@
 package br.com.criptovision.controller;
 
+import br.com.criptovision.service.CarteiraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carteira")
 public class CarteiraController {
+
+    @Autowired
+    private CarteiraService carteiraService;
+
+    @GetMapping("/total")
+    public String obterTotal() {
+        double total = carteiraService.calcularPatrimonioTotal();
+        return "Patrimônio Total Investido: R$ " + total;
+    }
 }
