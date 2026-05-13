d90a9545d24f0a54e855c29d4eacb2f0b299845e
xandao.dev <alexandrevitaldp@gmail.com>
2026-05-13 13:51:55 -0300

    criando a interface TransacaoRepository

---
+++ src/main/java/br/com/criptovision/repository/TransacaoRepository.java
@@ -0,0 +1,10 @@
+package br.com.criptovision.repository;
+
+import br.com.criptovision.model.Transacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+}
