ec0dc6d868e16db2a1649006389c1a989f7b05f1
xandao.dev <alexandrevitaldp@gmail.com>
2026-05-13 13:56:15 -0300

    implementando a classe de controle TransacaoController, com as marcações e injeções de dependencia

--- src/main/java/br/com/criptovision/controller/TransacaoController.java
+++ src/main/java/br/com/criptovision/controller/TransacaoController.java
@@ -1,4 +1,24 @@
 package br.com.criptovision.controller;
 
+import br.com.criptovision.model.Transacao;
+import br.com.criptovision.repository.TransacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transacoes")
 public class TransacaoController {
+
+    @Autowired
+    private TransacaoRepository repository;
+
+    @GetMapping
+    public List<Transacao> listarTodas() {
+        // vai no banco de dados, faz um select *, transforma tudo em lista e devolve
+        return repository.findAll();
+    }
 }
