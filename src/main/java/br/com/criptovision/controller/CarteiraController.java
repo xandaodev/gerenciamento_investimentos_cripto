abd3a6bc02ae8f1b78275f056397f72e173d7c0a
xandao.dev <alexandrevitaldp@gmail.com>
2026-05-14 09:40:41 -0300

    criando o obterResumo em CarteiraController

--- src/main/java/br/com/criptovision/controller/CarteiraController.java
+++ src/main/java/br/com/criptovision/controller/CarteiraController.java
@@ -1,5 +1,6 @@
 package br.com.criptovision.controller;
 
+import br.com.criptovision.dto.ResumoCarteiraDTO;
 import br.com.criptovision.service.CarteiraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +18,10 @@ public class CarteiraController {
     public String obterTotal() {
         double total = carteiraService.calcularPatrimonioTotal();
         return "Patrimônio Total Investido: R$ " + total;
+    }
+
+    @GetMapping("/resumo")
+    public ResumoCarteiraDTO obterResumo(){
+        return carteiraService.obterResumoGeral();
     }
 }
