0fcf5f2eacc0f1162946373d05f0af29e93d1264
xandao.dev <alexandrevitaldp@gmail.com>
2026-05-14 23:39:21 -0300

    apagando classes DAO uqe não tem mais serventia

--- src/main/java/br/com/criptovision/repository/LucroDAO.java
+++
@@ -1,7 +0,0 @@
-package br.com.criptovision.repository;
-
-public interface LucroDAO {
-    void salvarLucroRealizado(String ticker, double valorLucro);// guarda o lucro de uma venda
-    
-    double lerLucroTotal();//lê a soma de todos os lucros
-}
