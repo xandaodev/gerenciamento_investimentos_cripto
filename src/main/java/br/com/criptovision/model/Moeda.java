ff43d38cb68b93f0e077802bb613128f13e596e4
xandao.dev <alexandrevitaldp@gmail.com>
2026-05-14 23:11:58 -0300

    iniciando as alterações de double para BihDecimal, agora em Moeda e Transacao

--- src/main/java/br/com/criptovision/model/Moeda.java
+++ src/main/java/br/com/criptovision/model/Moeda.java
@@ -3,30 +3,33 @@ package br.com.criptovision.model;
 
 //essa clase representa uma criptomoeda dentro do sistema, ela armazena os dados de todas as compras e vendas
 
+import java.math.BigDecimal;
+
 public class Moeda {
     private String nome; // "bitcoin"
     private String ticker;// "BTC"
-    private double saldo;
-    private double precoMedio;
+    private BigDecimal saldo;
+    private BigDecimal precoMedio;
 
-    private double variacao24h;
+    private BigDecimal variacao24h;
 
     public Moeda(String nome, String ticker){
         this.nome = nome.toLowerCase();
         this.ticker = ticker.toUpperCase();
-        this.saldo = 0.0;
-        this.precoMedio = 0.0;
+        this.saldo = BigDecimal.ZERO;
+        this.precoMedio = BigDecimal.ZERO;
+        this.variacao24h = BigDecimal.ZERO;
     }
     
     //getter e setters
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
     public String getTicker() { return ticker; }
-    public double getSaldo() { return saldo; }
-    public void setSaldo(double saldo) { this.saldo = saldo; }
-    public double getPrecoMedio() { return precoMedio; }
-    public void setPrecoMedio(double precoMedio) { this.precoMedio = precoMedio; }
+    public BigDecimal getSaldo() { return saldo; }
+    public void setSaldo(BigDecimal saldo) { this.saldo = saldo; }
+    public BigDecimal getPrecoMedio() { return precoMedio; }
+    public void setPrecoMedio(BigDecimal precoMedio) { this.precoMedio = precoMedio; }
 
-    public double getVariacao24h(){ return variacao24h; }
-    public void setVariacao24h(double variacao24h){ this.variacao24h = variacao24h; }
+    public BigDecimal getVariacao24h(){ return variacao24h; }
+    public void setVariacao24h(BigDecimal variacao24h){ this.variacao24h = variacao24h; }
 }
