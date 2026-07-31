6d65fae4e3d58d1950d2c2eecd54c9837e055a7c
xandao.dev <alexandrevitaldp@gmail.com>
2026-07-31 12:52:19 -0300

    test: configura ambiente isolado para testes

--- src/test/java/br/com/criptovision/CriptovisionApiApplicationTests.java
+++ src/test/java/br/com/criptovision/CriptovisionApiApplicationTests.java
@@ -2,12 +2,13 @@ package br.com.criptovision;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CriptovisionApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
