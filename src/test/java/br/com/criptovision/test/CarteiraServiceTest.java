26e181190f24dec0ab0463aef2876cf1e7c65822
xandao.dev <alexandrevitaldp@gmail.com>
2026-07-31 13:39:37 -0300

    fix: ordena historico antes de reconstruir carteira

--- src/test/java/br/com/criptovision/test/CarteiraServiceTest.java
+++ src/test/java/br/com/criptovision/test/CarteiraServiceTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import br.com.criptovision.model.Carteira;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import br.com.criptovision.model.Moeda;
 import br.com.criptovision.model.Transacao;
 import br.com.criptovision.service.CarteiraService;
@@ -87,5 +92,37 @@ public class CarteiraServiceTest {
         double lucroPotencial = service.calcularLucroPotencial(btc, 70000.0);
 
         assertEquals(10000.0, lucroPotencial, 0.001);
+    }
+
+    @Test
+    public void deveReconstruirCarteiraEmOrdemCronologicaMesmoComHistoricoDesordenado() {
+        CarteiraService service = new CarteiraService();
+        Carteira carteira = new Carteira();
+
+        Transacao compra = new Transacao(
+            "BTC",
+            BigDecimal.ONE,
+            BigDecimal.valueOf(50000),
+            "COMPRA"
+        );
+        compra.setId(1L);
+        compra.setData(LocalDateTime.of(2026, 1, 1, 10, 0));
+
+        Transacao venda = new Transacao(
+            "BTC",
+            BigDecimal.ONE,
+            BigDecimal.valueOf(60000),
+            "VENDA"
+        );
+        venda.setId(2L);
+        venda.setData(LocalDateTime.of(2026, 1, 1, 11, 0));
+
+        List<Transacao> historicoDesordenado = List.of(venda, compra);
+
+        service.reconstruirCarteira(carteira, historicoDesordenado);
+
+        Moeda btc = carteira.obterMoeda("BTC", "Bitcoin");
+
+        assertEquals(0.0, btc.getSaldo().doubleValue(), 0.001);
     }
 }
